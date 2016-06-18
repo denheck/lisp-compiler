5c8b2b006d971be1394473fe12250991f75d0705
Dennis Heckman <denheck@gmail.com>
2016-06-18 07:53:46 -0500

    change index to tsx file

---
+++ index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from "./demo/components/App.tsx";
+
+ReactDOM.render(<App greeting={"Hello World"} />, document.getElementById("app"));
