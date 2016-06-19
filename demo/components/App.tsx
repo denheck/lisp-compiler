c64e74cad0f23c3596b5b30bea6ffdd147a2e3a6
Dennis Heckman <denheck@gmail.com>
2016-06-19 11:23:31 -0500

    update app

--- demo/components/App.tsx
+++ demo/components/App.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
 import Panel from './panels/Panel.tsx';
+import * as CodeMirror from "react-codemirror"
 
-const App = () =>
-    <div>
-        <h1>LISP to JavaScript Compiler</h1>
-        <div id="panels">
-            <Panel heading="Lisp">
-                <div>LISP PANEL</div>
-            </Panel>
-            <Panel heading="AST">
-                <div>AST PANEL</div>
-            </Panel>
-            <Panel heading="JS">
-                <div>JS PANEL</div>
-            </Panel>
-        </div>
-    </div>;
+type AppState = { lisp: string, ast: string, javascript: string };
+
+class App extends React.Component<{}, AppState> {
+    state: AppState;
+
+    constructor() {
+        super();
+        this.state = { lisp: "// Code", ast: "", javascript: "" };
+    }
+
+    private onChange(newLisp: string): void {
+        this.setState({ lisp: newLisp } as AppState);
+    }
+
+    render() {
+        return <div>
+            <h1>LISP to JavaScript Compiler</h1>
+            <div id="panels">
+                <Panel heading="Lisp">
+                    <CodeMirror value={this.state.lisp} onChange={this.onChange} />
+                </Panel>
+                <Panel heading="AST">
+                    <div>{this.state.ast}</div>
+                </Panel>
+                <Panel heading="JS">
+                    <div>{this.state.javascript}</div>
+                </Panel>
+            </div>
+        </div>;
+    }
+}
 
 export default App;
