fe1a39c9d9a06ce39735578cb3bebd064b5cfaf3
Dennis Heckman <denheck@gmail.com>
2016-06-19 10:08:01 -0500

    move index file into demo

--- index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from "./demo/components/App.tsx";
-
-ReactDOM.render(<App greeting={"Hello World"} />, document.getElementById("app"));
